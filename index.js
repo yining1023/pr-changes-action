944dec3e45d66c9f94642584a601ad189b8ed8fa
Robin Andeer <robin.andeer@gmail.com>
2025-03-20 14:30:11 -0400

    [revert] overwrite pr body every time (#8)

--- index.js
+++ index.js
@@ -80,13 +80,7 @@ const run = async () => {
       });
     }
 
-    const { data: releasePr } = await octokit.rest.pulls.get({
-      owner,
-      repo,
-      pull_number: prNumber,
-    });
-    const body = releasePr.body;
-    const newBody = `We are ready for QA. Shoutout to @design-team & @product-eng-team for all the updates!:tada:\r\n${changes}\r\n${body}\r\nPR: [#${prNumber}](https://github.com/runwayml/app/pull/${prNumber})\r\n`;
+    const newBody = `We are ready for QA. Shoutout to @design-team & @product-eng-team for all the updates!:tada:\r\n${changes}\r\nPR: [#${prNumber}](https://github.com/runwayml/app/pull/${prNumber})\r\n`;
 
     await octokit.rest.pulls.update({
       owner,
